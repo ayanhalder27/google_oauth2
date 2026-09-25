ecb1a6e0e46d56d051074c5a6a387c0b35e3d836
Ayan Halder <114690741+ayanhalder27@users.noreply.github.com>
2026-09-26 01:07:24 +0600

    bug fixed & improvements

--- src/main/java/com/example/assignment/aspect/LoggingAspect.java
+++ src/main/java/com/example/assignment/aspect/LoggingAspect.java
@@ -1,21 +1,25 @@
 package com.example.assignment.aspect;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 @Aspect
 @Component
-public class LoggingAspect implements LogoutHandler {
+public class LoggingAspect implements LogoutHandler, AuthenticationEntryPoint {
     private static final Logger logger = Logger.getLogger("LoggingAspect");
 
     @PostConstruct
@@ -39,6 +43,12 @@ public class LoggingAspect implements LogoutHandler {
         if (authentication != null){
             logger.info("User logged out: " + authentication.getName());
         }
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        logger.warning("Unauthorized access attempt: " + request.getMethod() + " " + request.getRequestURI());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication required");
     }
 }
 
